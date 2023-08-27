6cd3d0c997888623e421ab5d9969142d1047405b
root <root@DESKTOP-SOESFLS>
2023-08-27 12:11:26 -0500

    Configure GitHub Actions to run manually

--- Infrastructure/scripts/ansibleLaunchConfiguration.sh
+++ Infrastructure/scripts/ansibleLaunchConfiguration.sh
@@ -14,8 +14,7 @@ sudo apt install ansible -y
 cd /srv/app/Infrastructure/conf_mgmt_ansible/playbooks
 sudo su #Just for ec2 machine, not container
 ansible-playbook 01-image-build-and-push.yml
-exit
-#Inside the playbook, a COPY module copies file "index.nginx-debian.html" to "/var/www/html/" directory
+exit #Just for ec2 machine, not container
 
 # #Run nginx-test-app server
 # cd /srv/app/test-app
