f872072d336347503a00900fa5f2193cd26c8367
root <root@DESKTOP-SOESFLS>
2023-08-27 19:48:14 -0500

    Configure Docker run

--- Infrastructure/scripts/ansibleLaunchConfiguration.sh
+++ Infrastructure/scripts/ansibleLaunchConfiguration.sh
@@ -7,13 +7,19 @@ mkdir -p /srv/app
 sudo chmod 755 /srv/app
 git clone https://github.com/MateoMatta/DevOps-Interview-AC /srv/app
 
+cd
+mkdir test
+chmod 644 test
+cd test
+
+
 #Install ansible
 sudo apt install ansible -y
 
 #Run Ansible playbook
 cd /srv/app/Infrastructure/conf_mgmt_ansible/playbooks
 # sudo su #Just for ec2 machine, not container
-ansible-playbook 01-image-build-and-push.yml
+# ansible-playbook 01-image-build-and-push.yml
 # exit #Just for ec2 machine, not container
 
 # #Run nginx-test-app server
