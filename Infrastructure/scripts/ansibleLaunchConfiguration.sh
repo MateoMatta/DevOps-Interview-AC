2daf264227383b3b5683bf566cc227c8f98d3cea
root <root@DESKTOP-SOESFLS>
2023-08-28 10:37:40 -0500

    First app-test deploy test

--- Infrastructure/scripts/ansibleLaunchConfiguration.sh
+++ Infrastructure/scripts/ansibleLaunchConfiguration.sh
@@ -12,6 +12,21 @@ mkdir test
 chmod 644 test
 cd test
 
+sudo apt update -y
+sudo apt install git -y
+sudo mkdir /home/runner/app_content
+sudo chmod 755 /home/runner/app_content
+echo ''
+sudo cp ./Dockerfile /home/runner/app_content/Dockerfile
+sudo ls /home/runner/app_content/
+#Install ansible
+sudo apt install ansible -y
+sudo ls /home/runner/work/DevOps-Interview-AC/DevOps-Interview-AC/Infrastructure/conf_mgmt_ansible/playbooks
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt-cache policy docker-ce
+sudo apt install docker-ce -y
 #Install Docker
 
 #Install ansible
