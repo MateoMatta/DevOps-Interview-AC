2fe186dcb2de8433d9c97564a89fb191a00232a0
root <root@DESKTOP-SOESFLS>
2023-08-28 12:53:29 -0500

    First app-test deploy test

--- Infrastructure/scripts/ansibleLaunchConfiguration.sh
+++ Infrastructure/scripts/ansibleLaunchConfiguration.sh
@@ -27,6 +27,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt-cache policy docker-ce
 sudo apt install docker-ce -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo docker run -d -p 80:80 --name httpd httpd
+#AWS_ACCESS_KEY_ID=123 AWS_SECRET_ACCESS_KEY=3456 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 729158664723.dkr.ecr.us-east-1.amazonaws.com
+#sudo docker images
+#sudo docker run -d -p 8080:80 --name main-applications-registry 729158664723.dkr.ecr.us-east-1.amazonaws.com/main-applications-registry
+#sudo docker run -d -p 80:80 --name httpd httpd
+#curl localhost:8080
+
+
 #Install Docker
 
 #Install ansible
