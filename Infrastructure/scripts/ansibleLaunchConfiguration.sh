4be6a425a4b5a83e0fbf575c4e8dbb474b62c38f
root <root@DESKTOP-SOESFLS>
2023-08-29 18:27:38 -0500

    Fifth app-test deploy test

--- Infrastructure/scripts/ansibleLaunchConfiguration.sh
+++ Infrastructure/scripts/ansibleLaunchConfiguration.sh
@@ -23,16 +23,24 @@ sudo apt install ansible -y
 #Run Ansible playbook
 ansible-playbook /srv/app/Infrastructure/conf_mgmt_ansible/playbooks/02-image-pull-and-deploy.yml
 
+#EDIT HERE to change docker image webpage
+
+#AWS_ACCESS_KEY_ID=123 AWS_SECRET_ACCESS_KEY=3456 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 729158664723.dkr.ecr.us-east-1.amazonaws.com
+#sudo docker run --name main-applications-registry -p 80:80 -d 729158664723.dkr.ecr.us-east-1.amazonaws.com/main-applications-registry
+# ----------- or -----------------
+sudo docker run -d -p 80:80 --name httpd httpd
+
+
+
+
+
 # ansible-playbook /srv/app/Infrastructure/conf_mgmt_ansible/playbooks/02-image-pull-and-deploy.yml -e ansible_password='{{ lookup("env", "ANSIBLE_PASSWORD", "OTHER_PASSWORD") }}'
 #ansible-playbook /srv/app/Infrastructure/conf_mgmt_ansible/playbooks/02-image-pull-and-deploy.yml
 
 #sudo ls /home/runner/work/DevOps-Interview-AC/DevOps-Interview-AC/Infrastructure/conf_mgmt_ansible/playbooks
 
 
-#AWS_ACCESS_KEY_ID=123 AWS_SECRET_ACCESS_KEY=3456 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 729158664723.dkr.ecr.us-east-1.amazonaws.com
-#sudo docker run --name main-applications-registry -p 80:80 -d 729158664723.dkr.ecr.us-east-1.amazonaws.com/main-applications-registry
 #sudo docker images
-#sudo docker run -d -p 80:80 --name httpd httpd
 #curl localhost:8080
 
 
