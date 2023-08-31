3b8f8d982fa982f308cfb1b099de7f4a730cffa3
root <root@DESKTOP-SOESFLS>
2023-08-31 11:14:11 -0500

    Automatize terraform deploy with containers

--- Infrastructure/conf_terraform/main.tf
+++ Infrastructure/conf_terraform/main.tf
@@ -218,9 +218,38 @@ resource "aws_s3_bucket_versioning" "versioning_for_s3Bucket" {
   }
 }
 
-module "rds_example_complete-postgres" {
-  source  = "terraform-aws-modules/rds/aws//examples/complete-postgres"
-  version = "6.1.1"
+resource "random_string" "postgres_db_demo-db-password" {
+  length  = 32
+  upper   = true
+  number  = true
+  special = false
+}
+
+resource "aws_security_group" "postgres_db_demo" {
+  vpc_id      = aws_vpc.demo-vpc.id
+  name        = "postgres_db_demo"
+  description = "Allow all inbound for Postgres"
+ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_db_instance" "postgres_db_demo" {
+  identifier             = "postgres_db_demo"
+  name                   = "postgres_db_demo"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "12.5"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.postgres_db_demo.id]
+  username               = "sameed"
+  password               = "random_string.postgres_db_demo-db-password.result}"
 }
 
 # resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
