ca32cca7d64a7484dce0a44361adba28b991d37f
root <root@DESKTOP-SOESFLS>
2023-09-01 11:28:28 -0500

    Workflow for CI/CD completed

--- Infrastructure/scripts/getAsgMachinesIps.sh
+++ Infrastructure/scripts/getAsgMachinesIps.sh
@@ -10,14 +10,22 @@ for ID in $ids;
 do
     IP=$(aws ec2 describe-instances --instance-ids $ID --region $2 --query Reservations[].Instances[].PublicIpAddress --output text)
     echo $IP
-    export COMMAND_FOR_MACHINE= "touch $HOME/accessKey.txt; echo "$3" > $HOME/accessKey.txt"
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $COMMAND_FOR_MACHINE > /dev/null 2>&1
-    export COMMAND_FOR_MACHINE="touch $HOME/secAccessKey.txt; echo "$4" > $HOME/secAccessKey.txt"
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $COMMAND_FOR_MACHINE > /dev/null 2>&1
 
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP 'AWS_ACCESS_KEY_ID=$(cat accessKey.txt) AWS_SECRET_ACCESS_KEY=$(cat secAccessKey.txt) aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 729158664723.dkr.ecr.us-east-1.amazonaws.com' > /dev/null 2>&1
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP 'sudo docker run --name main-applications-registry -p 80:80 -d 729158664723.dkr.ecr.us-east-1.amazonaws.com/main-applications-registry' > /dev/null 2>&1
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP 'rm $HOME/accessKey.txt; rm $HOME/secAccessKey.txt; echo "Initializing containers..."'
+    export MACHINE_COMMAND='touch accessKey.txt; echo '$3' > accessKey.txt'
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
+
+    export MACHINE_COMMAND='touch secAccessKey.txt; echo '$4' > secAccessKey.txt; ls; pwd'
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
+
+    export MACHINE_COMMAND='AWS_ACCESS_KEY_ID=$(cat accessKey.txt) AWS_SECRET_ACCESS_KEY=$(cat secAccessKey.txt) aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 729158664723.dkr.ecr.us-east-1.amazonaws.com'
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
+    
+    export MACHINE_COMMAND='sudo docker run --name main-applications-registry -p 80:80 -d 729158664723.dkr.ecr.us-east-1.amazonaws.com/main-applications-registry'
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
+    
+    export MACHINE_COMMAND='rm accessKey.txt; rm secAccessKey.txt; echo "Initializing containers..."'
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
+
 
     ips="$ips,$IP"
 done
