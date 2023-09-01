8bb4130db829b43bdb31819ee5a769349e4faba0
root <root@DESKTOP-SOESFLS>
2023-09-01 15:32:05 -0500

    Workflow for CI/CD made successfully

--- Infrastructure/scripts/runDockerContainers.sh
+++ Infrastructure/scripts/runDockerContainers.sh
@@ -1,8 +1,8 @@
 
 export AWS_ACCESS_KEY_IDD=$(cat /home/ubuntu/accessKey.txt)
 export AWS_SECRET_ACCESS_KEYY=$(cat  /home/ubuntu/secAccessKey.txt)
-cd /root
-apt update -y; apt install unzip curl -y;curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip";unzip awscliv2.zip;./aws/install --update
+# cd /root
+# apt update -y; apt install unzip curl -y;curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip";unzip awscliv2.zip;./aws/install --update
 aws --version
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_IDD
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEYY
