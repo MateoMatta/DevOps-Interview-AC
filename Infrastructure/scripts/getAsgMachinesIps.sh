43e9590e54709fe171f64edd2996de42d262b15f
root <root@DESKTOP-SOESFLS>
2023-09-01 14:11:31 -0500

    Workflow for CI/CD made successfully

--- Infrastructure/scripts/getAsgMachinesIps.sh
+++ Infrastructure/scripts/getAsgMachinesIps.sh
@@ -11,19 +11,18 @@ do
     IP=$(aws ec2 describe-instances --instance-ids $ID --region $2 --query Reservations[].Instances[].PublicIpAddress --output text)
     echo $IP
     echo "Initializing containers..."
-    export MACHINE_COMMAND='touch accessKey.txt; echo '$3' > accessKey.txt'
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND 
+    export MACHINE_COMMAND="touch accessKey.txt; echo "$3" > accessKey.txt"
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
 
-    export MACHINE_COMMAND='touch secAccessKey.txt; echo '$4' > secAccessKey.txt; ls; pwd'
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND 
+    export MACHINE_COMMAND="touch secAccessKey.txt; echo $4 > secAccessKey.txt; ls; pwd"
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
 
-    export MACHINE_COMMAND='sh /srv/app/Infrastructure/scripts/runDockerContainers.sh'
-    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND 
+    export MACHINE_COMMAND="sh /srv/app/Infrastructure/scripts/runDockerContainers.sh"
+    sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
 
     # export MACHINE_COMMAND='rm accessKey.txt; rm secAccessKey.txt'
-    # sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND 
+    # sudo ssh -oStrictHostKeyChecking=no -i "/home/runner/candidate.pem" ubuntu@$IP $MACHINE_COMMAND
 
 
     ips="$ips,$IP"
 done
-
